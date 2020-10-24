7fd274eab31669d475e7b10a76c2cde633e4253a
ravula7 <ravula7@ad.unc.edu>
2020-10-24 08:55:15 -0400

    commit

--- src/a7/AVLTree.java
+++ src/a7/AVLTree.java
@@ -2,10 +2,28 @@ package a7;
 
 public class AVLTree<T extends Comparable<T>> implements SelfBalancingBST<T> {
     // Put your fields here.
-    
+    AVLTree root;
+    AVLTree left;
+    AVLTree right;
+
     public AVLTree() {
         // Your code for constructor here.
+        SelfBalancingBST<T> avlTree = new AVLTree<T>();
+        this.root = root;
+        this.left = root.left;
+        this.right = root.right;
     }
+    /*
+    private T _element;
+	private BST<T> _left;
+	private BST<T> _right;
+
+	public NonEmptyBST(T element) {
+		_left = new EmptyBST<T>(); //empty left bst created -- left child
+		_right = new EmptyBST<T>(); //empty right bst created -- right child
+		_element = element; //element created T
+	}
+     */
 
     /**
      *
@@ -13,7 +31,7 @@ public class AVLTree<T extends Comparable<T>> implements SelfBalancingBST<T> {
      * AVLTree root for rotated result.
      */
     
-     private void rotateLeft() {//AVLTree<T>
+     private AVLTree<T> rotateLeft() {
          // You should implement left rotation and then use this 
          // method as needed when fixing imbalances.
      }
@@ -24,7 +42,7 @@ public class AVLTree<T extends Comparable<T>> implements SelfBalancingBST<T> {
      * AVLTree root for rotated result.
      */ 
      
-     private void rotateRight() {//AVLTree<T>
+     private AVLTree<T> rotateRight() {
          // You should implement right rotation and then use this 
          // method as needed when fixing imbalances.
      }
@@ -37,54 +55,120 @@ public class AVLTree<T extends Comparable<T>> implements SelfBalancingBST<T> {
 
     @Override
     public int height() {
-        return 0;
+         return size()-1;
     }
 
     @Override
     public int size() {
-        return 0;
+        int sizeOfLeft=0;
+        int sizeOfRight=0;
+        if(this.left !=null){
+            sizeOfLeft = this.left.size();
+        }
+        if(this.right != null){
+            sizeOfRight=this.right.size();
+        }
+        if(root == null){
+            return 0;
+        }
+        return sizeOfLeft+sizeOfRight+1;
     }
 
     @Override
     public SelfBalancingBST<T> insert(T element) {
-        return null;
+        if (element.compareTo((T) root) >= 0) { //recurse to the right because element is larger or the same
+            right = (AVLTree) right.insert(element);
+        }
+        else if (element.compareTo((T) root) < 0) { //recurse to the left because element is smaller
+            left = (AVLTree) left.insert(element);
+        }
+        //check for imbalances and rotate if necessary
+        if(left.height()-right.height() != 1 ||left.height()-right.height() != 0 || left.height()-right.height() != -1){
+
+        }
+        return this;
     }
 
     @Override
     public SelfBalancingBST<T> remove(T element) {
-        return null;
+        if (element.compareTo(root) > 0) { //recurse to the right because element is larger
+            right = (AVLTree) right.remove(element);
+        }
+        else if (element.compareTo(root) <0) { //recurse to the left because element is smaller
+            left = (AVLTree) left.remove(element);
+        }
+        else if(left.isEmpty() && right.isEmpty()){
+            EmptyBST<T> emptyTree = new EmptyBST<T>();
+            return emptyTree;
+        }
+        else if(!left.isEmpty() && right.isEmpty()){
+            return left;
+        }
+        else if(left.isEmpty() && !right.isEmpty()){
+            return right;
+        }
+        else if (!left.isEmpty() && !right.isEmpty()){
+            T minimum = (T) right.findMin();
+            root = (AVLTree) minimum;
+            right = (AVLTree) right.remove(minimum);
+        }
+        return this;
     }
 
     @Override
     public T findMin() {
-        return null;
+        T minimum = (T) root;
+        if(!left.isEmpty()) {
+            minimum = (T) left.findMin();
+        }
+        else if(left.isEmpty()){
+            minimum = (T) this.root;
+        }
+        return minimum;
     }
 
     @Override
     public T findMax() {
-        return null;
+        T maximum =  (T) root;
+        if(!right.isEmpty()) {
+            maximum = (T) right.findMax();
+        }
+        else if(right.isEmpty()){
+            maximum = (T) this.root;
+        }
+        return maximum;
     }
 
     @Override
     public boolean contains(T element) {
-        return false;
+        if(this.isEmpty()){ //when the tree is empty, it contains no elements
+            return false;
+        }
+        else if(element.compareTo((T) root) == 0){ //found the element!
+            return true;
+        }
+        else if(element.compareTo((T) root) > 0){ //need to recurse right to find the element
+            return getRight().contains(element); //return true or false whether the right side contains the element or not
+        }
+        else if(element.compareTo(getValue()) < 0){ //need to recurse left to find the element
+            return getLeft().contains(element); //return true or false whether the left side contains the element or not
+        }
+        return false; //otherwise
     }
 
     @Override
     public T getValue() {
-        return null;
+        return (T) root; //or is it null as it was originally?
     }
 
     @Override
     public SelfBalancingBST<T> getLeft() {
-        return null;
+        return left;
     }
 
     @Override
     public SelfBalancingBST<T> getRight() {
-        return null;
+        return right;
     }
-
-
 
 }
